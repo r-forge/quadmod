fcfad30dfb35871e2f83239b00d7fd8cf00f34f9
tdhock (Toby Dylan Hocking) <tdhock5@gmail.com>
2011-04-25 23:20:04 +0000

    operator overloading for more clear problem specification
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/quadmod@3 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/translator.R
+++ pkg/R/translator.R
@@ -11,7 +11,7 @@ make.ids <- structure(function
     d <- vars[[N]]
     n <- prod(d)
     end <- i+n-1
-    a <- array(i:end,d)
+    a <- structure(array(i:end,d),class="id",variable=N)
     i <- end+1
     ids[[N]] <- a
   }
@@ -20,7 +20,88 @@ make.ids <- structure(function
 ### construct the standard form matrices.
 },ex=function(){
   vars <- make.ids(xi=5,beta0=1,beta=2)
+  with(vars, xi >= 0)
 })
+
+">=.id" <- function(a,constant){
+### makes a list of inequality constraints, one for each variable
+  a[] >= constant
+}
+
+"*.id" <- function(a,constant){
+### multiply coefficients, returning a linear combination.
+  sum(a[]) * constant
+}
+
+"*.lincomb" <- function(vars,x){
+### multiply coefficients, returning a linear combination
+  if(length(x)==1)x <- rep(x,length(vars))
+  stopifnot(length(vars)==length(x))
+  for(i in seq_along(vars)){
+    vars[[i]]$coef <- vars[[i]]$coef * x[i]
+  }
+  structure(vars,class="lincomb")
+}
+
+sum.id <- function(a,...){
+### sum all variables, returning a linear combination
+  sum(a[])
+}
+
+">=.lincomb" <- function(vars,constant){
+### make an inequality constraint from a linear combination
+  arglist <- c(list("geq",constant),vars)
+  do.call(constrain,arglist)
+}
+
+"==.lincomb" <- function(vars,constant){
+### make an equality constraint from a linear combination
+  arglist <- c(list("eq",constant),vars)
+  do.call(constrain,arglist)
+}
+
+">=.lincomblist" <- function(lcs,constant){
+### make a list of inequality constraints from a list of linear combinations
+  lapply(lcs,function(lc)lc>=constant)
+}
+
+sum.lincomblist <- function(lcs,...){
+### collapse a list of linear combinations into a single linear combination
+  structure(lapply(lcs,"[[",1),class="lincomb")
+}
+
+"+.lincomb" <- function(v1,v2){
+### combine optimization variables in a linear combination
+  if(class(v2)%in%c("id","lincomblist"))v2 <- sum(v2)
+  structure(c(v1,v2),class="lincomb")
+}
+
+### Model: define a less than operator for a linear combination of
+### optimization variables, which returns a constraint. Define an
+### addition operator for a linear combination, which returns another
+### linear combination, which is just a list of variables and
+### coefficients. 
+
+"[.id" <- function(var,i,j){
+### Make some variables from an id matrix.
+  N <- attr(var,"variable")
+  linear.combinations <- list()
+  if(missing(i))i <- 1:nrow(var)
+  if(length(dim(var))==1){
+    for(ii in i){
+      lc <- structure(list(variable(N,ii)),class="lincomb")
+      linear.combinations <- c(linear.combinations,list(lc))
+    }
+  }else{
+    if(missing(j))j <- 1:ncol(var)
+    for(ii in i)for(jj in j){
+      lc <- structure(list(variable(N,ii,jj)),class="lincomb")
+      linear.combinations <- c(linear.combinations,list(lc))
+    }
+  }
+  structure(linear.combinations,class="lincomblist")
+}
+
 constrain <- structure(function
 ### Make a constraint list. Write each constraint as c_1 v_1 + ... +
 ### c_n v_n (in)equality constant, where c_i are real coefficients and
@@ -81,7 +162,7 @@ print.constraint <- function(x,...){
   for(v in x$variables){
     print(v)
   }
-  key <- c(geq=">=",eq="=")
+  key <- c(geq=">=",eq="==")
   cat(key[x$equality],x$constant,"\n")
 }
 standard.form.constraints <- structure(function
@@ -103,7 +184,7 @@ standard.form.constraints <- structure(function
     constraint <- ordered.constraints[[cn]]
     for(vn in seq_along(constraint$variables)){
       v <- constraint$variables[[vn]]
-      a <- ids[[v$variable]]
+      a <- unclass(ids[[v$variable]])
       if(is.null(a))stop(sprintf("variable %s not found",v$variable))
       A.row <- if(is.null(v$j)){
         a[v$i]
@@ -119,13 +200,7 @@ standard.form.constraints <- structure(function
   ## example: optimization variable \alpha\in\RR^3 subject to the
   ## constraint that it must be on the standard simplex:
   opt.vars <- make.ids(alpha=3)
-  constraints <- list(constrain("geq",0,variable("alpha",1)),
-                      constrain("geq",0,variable("alpha",2)),
-                      constrain("geq",0,variable("alpha",3)),
-                      constrain("eq",1,
-                                variable("alpha",1),
-                                variable("alpha",2),
-                                variable("alpha",3)))
+  constraints <- with(opt.vars,c(alpha >= 0,list(sum(alpha) == 1)))
   standard.form.constraints(constraints,opt.vars)
 
   ## linear svm example
@@ -137,17 +212,10 @@ standard.form.constraints <- structure(function
   n <- nrow(x)
   
   vars <- make.ids(slack=n,intercept=1,normal=p)
-  constraints <- list()
+  constraints <- vars$slack >= 0
   for(i in 1:n){
-    cargs <- list("geq",1,
-                  variable("slack",i),
-                  variable("intercept",coef=y[i]))
-    for(j in 1:p){
-      cargs <- c(cargs,list(variable("normal",j,coef=y[i]*x[i,j])))
-    }
-    constraints <-
-      c(constraints,list(constrain("geq",0,variable("slack",i)),
-                         do.call(constrain,cargs)))
+    ivars <- with(vars,intercept*y[i] + sum(normal)*(x[i,]*y[i]) + slack[i])
+    constraints <- c(constraints,list(ivars >= 1))
   }
   solver.args <- standard.form.constraints(constraints,vars)
   n.vars <- length(unlist(vars))
