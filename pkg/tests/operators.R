f0b235e5caf29e9e2bcffa166908007979c4e4a5
tdhock (Toby Dylan Hocking) <tdhock5@gmail.com>
2013-08-23 09:39:42 +0000

    operator tests
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/quadmod@15 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/tests/operators.R
@@ -0,0 +1,45 @@
+library(quadmod)
+vars <- make.ids(x=3)
+
+## each variable must be positive.
+positive <- vars$x >= 0
+stopifnot(is.list(positive))
+stopifnot(sapply(positive, is.constraint))
+stopifnot(length(positive)==3)
+
+## the sum of all x must be positive.
+sum.pos <- sum(vars$x) >= 0
+stopifnot(is.constraint(sum.pos))
+stopifnot(length(sum.pos$variables)==3)
+
+## the sum of all x must be 1.
+sum.one <- sum(vars$x) == 1
+stopifnot(is.constraint(sum.one))
+stopifnot(length(sum.one$variables)==3)
+
+## this linear combination must be equal to 1.
+lc.one <- vars$x * c(-1,1,-2) == 1
+stopifnot(is.constraint(lc.one))
+stopifnot(length(lc.one$variables)==3)
+
+## this variable must be positive.
+one.pos <- vars$x[1] >= 0
+stopifnot(is.constraint(one.pos))
+stopifnot(length(one.pos$variables)==1)
+
+## these two variables must be positive.
+two.pos <- vars$x[c(1,3)] >= 0
+stopifnot(sapply(two.pos, is.constraint))
+stopifnot(length(two.pos)==2)
+
+## each of these variables times these values must be positive.
+each.lc.pos <- vars$x[] * c(-1,1,1) >= 1
+stopifnot(is.list(each.lc.pos))
+stopifnot(sapply(each.lc.pos, is.constraint))
+
+## svm variable combination
+svm <- make.ids(margin=1, weight=2)
+svmc <- svm$weight*c(-1,1) + svm$margin*-1 >= 1
+stopifnot(is.constraint(svmc))
+stopifnot(length(svmc$variables)==3)
+
